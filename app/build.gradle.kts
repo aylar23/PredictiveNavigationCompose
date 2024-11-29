464cde951515dfcb939e604f451303e448b84e13
aylar23 <aylarbayram23@gmail.com>
2024-11-29 16:57:31 +0500

    Implement custom predictive back animation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.aylar.predictivenavigationcompose"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.aylar.predictivenavigationcompose"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
